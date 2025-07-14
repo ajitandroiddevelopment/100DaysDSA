7606a955751af865c21e40b7f97760ceebad612f
AJIT MAURYA <ajitmaurya.stud@gmail.com>
2025-07-14 19:09:07 +0530

    factorial day 1

---
+++ src/Day_One_to_Ten/Day1_factorial.java
@@ -0,0 +1,18 @@
+package Day_One_to_Ten;
+
+public class Day1_factorial {
+
+    public static int factorial(int n){
+        if(n==0 || n==1)// base case
+            return 1;
+        else
+            return n*factorial(n-1); // recursive call
+
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = factorial(number);
+        System.out.println("Factorial of "+number+ " is " +result);
+    }
+}
