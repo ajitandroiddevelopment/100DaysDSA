5f686b3c2b7b010ac62cfb79473b9063c3cbb40d
AJIT MAURYA <ajitmaurya.stud@gmail.com>
2025-07-29 04:02:51 +0530

    start

---
+++ src/Day12/SubarrayWithGivenSum.java
@@ -0,0 +1,28 @@
+package Day12;
+
+public class SubarrayWithGivenSum {
+
+    public static boolean hasSubarrayWithSum(int[] arr, int target){
+        int left =0 , right=0,sum=0;
+
+        while (right < arr.length){
+
+            sum = sum + arr[right];
+            // shrink window if the sum>target
+            while (sum>target && left <= right){
+                sum = sum -arr[left];
+                left++;
+            }
+            // check for the match(target is equal to sum or not
+            if (sum==target) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int target = 11;
+
+        System.out.println("Array is Having subArray with sum " +target+ " --> " +hasSubarrayWithSum(arr,target));
+    }
+}
